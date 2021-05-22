a5ccd0ef5c9655db4385d285a899f138f8571399
mo0777lucky <beautifulsea08264@gmail.com>
2021-05-22 21:42:05 +0900

    画像投稿機能の実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -16,4 +16,12 @@ class ProductsController < ApplicationController
   def show
   end
 
+  private
+  def product_params
+    params.require(:product).permit(:title, :category_id, :description, :prefecture_id, :municipality, :image).merge(user_id: current_user.id)
+  end
+
+
+
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,15 @@
 class Product < ApplicationRecord
+  
+  with_options presence: true do
+    validates :title
+    validates :category_id
+    validates :description
+    validates :prefecture_id
+    validates :municipality
+    validates :image
+  end
+
+  belongs_to :user
+  has_many :comments, dependent: :destroy
+  has_one_attached :image
 end
